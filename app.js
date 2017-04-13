04eaff0e59fea844c7747f93f3b2b98b061983f5
KevinKJ Lin(林楷峻) <KevinKJ_Lin@corpnet.asus>
2017-04-13 17:21:00 +0800

    Modify listen port

--- app.js
+++ app.js
@@ -22,6 +22,8 @@ app.get('/', function(req, res) {
 app.use('/restful', todoRouter);
 app.use('/restful',express.static(__dirname+'/public'));
 
-app.listen(3000,function(){
-    console.log('Ready...for 3000');
+
+app.set('port', (process.env.PORT || 5000));
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
